6509ebae5d96950a1d2c25dfd7bf4b592d40caa2
Stephen Lutes <6340474+stephenlutes@users.noreply.github.com>
2023-11-07 08:45:24 -0800

    Add parsing input data into lists of integers
    Adds a factory method that takes a seperating charactor. That charactor is
    then used by the returned parse method to parse the input data into a list
    of lists of integers.

--- src/northpole/parsers.py
+++ src/northpole/parsers.py
@@ -1,3 +1,37 @@
+from typing import Callable
+
+
+def int_list_parser(seperator: str) -> Callable:
+    """
+    Create a parser instance that will parse strings into a list of integers based on a
+    seperating character.
+
+    Example:
+        Providing ',' as the seperating character, the string '1,4,7' would be parsed as
+        [1, 4, 7].
+
+    Args:
+        seperator (str): The character to split each string on.
+
+    Returns:
+        Callable: The parser function.
+    """
+
+    def parse(data: str) -> list[list[int]]:
+        """
+        Take the input data and parse each line into a list of integers.
+
+        Args:
+            data (str): The input data for the challenge.
+
+        Returns:
+            list[list[int]]: The list of parsed lists of integers.
+        """
+        return [[int(x) for x in line.split(seperator)] for line in data.splitlines()]
+
+    return parse
+
+
 def parse_string(data: str) -> str:
     """
     Take the input data and parse it into a single string. The result is everything
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,6 +1,22 @@
+from typing import Callable
+
 import pytest
 
-from northpole.parsers import parse_string
+from northpole.parsers import int_list_parser, parse_string
+
+
+@pytest.mark.parametrize(
+    "data,expected",
+    [
+        ("1-2-3", [[1, 2, 3]]),
+        ("4-5-6\n", [[4, 5, 6]]),
+        ("7-8-9\n0-1-2", [[7, 8, 9], [0, 1, 2]]),
+    ],
+)
+def test_int_list_parser(data: str, expected: list[list[int]]) -> None:
+    parser: Callable = int_list_parser("-")
+
+    assert parser(data) == expected
 
 
 @pytest.mark.parametrize(
